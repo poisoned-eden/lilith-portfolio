e37b2f97f2e8d81cf47a848804f9a4902d97b7a3
poisoned-eden <poisoned.eden@gmail.com>
2023-08-27 20:28:59 +0100

    removes unneeded row & cols, adds id as card key

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-export default function Project({id, title, repoLink, deployedLink, screenshot, alt, description}) {
-    return (
-        <Card className='mt-5'>
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                    {description}
-                </Card.Text>
-                <Row>
-                    
-                    <Col>
-                        <Button href={repoLink} target='_blank'>Link to Repo</Button>
-                    </Col>
-                    <Col>
-                        <Button href={deployedLink} target='_blank'>Link to deployed project</Button>
-                    </Col>
-                </Row>
-            </Card.Body>
-            <Card.Img src={screenshot} alt={alt} variant='bottom' />
-        </Card>
+export default function Project({
+	id,
+	title,
+	repoLink,
+	deployedLink,
+	screenshot,
+	alt,
+	description,
+}) {
+	return (
+		<Card className="mt-5" key={id}>
+			<Card.Body>
+				<Card.Title>{title}</Card.Title>
+				<Card.Text>{description}</Card.Text>
+				<Button href={repoLink} target="_blank">
+					Link to Repo
+				</Button>
+				<Button href={deployedLink} target="_blank">
+					Link to deployed project
+				</Button>
+			</Card.Body>
+			<Card.Img src={screenshot} alt={alt} variant="bottom" />
+		</Card>
 
-        // <div key={id}>
-        //     <h4>{title}</h4
-        // </div>
-    );
+		// <div key={id}>
+		//     <h4>{title}</h4
+		// </div>
+	);
 }
