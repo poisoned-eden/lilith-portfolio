fa60f9184b745e3b0b348de452e7ffb7a840625e
poisoned-eden <poisoned.eden@gmail.com>
2023-08-27 18:34:35 +0100

    adds to Project: description and bootstrap compos

--- src/assets/myProjects.js
+++ src/assets/myProjects.js
@@ -8,14 +8,16 @@ export const myProjects = [
         title: 'something-swift-svg-logo-generator',
         repoLink: 'https://github.com/poisoned-eden/something-swift-svg-logo-generator',
         deployedLink: false,
-        screenshot: ''
+        screenshot: '',
+        description: 'A node.js command line interface that generates an SVG file that fits input requirements. This was created to practice object oriented programming, use of Classes within JS, and Jest Tests.'
     },
     {
         id: 2,
         title: 'echo-social-network-api',
         repoLink: 'https://github.com/poisoned-eden/echo-social-network-api',
         deployedLink: false,
-        screenshot: ''
+        screenshot: '',
+        description: 'an API for creating users and their friends, and the users thoughts and reactions to them. Buildin using Express.js and MongoDB'
     },
     {
         id: 3,
@@ -23,7 +25,8 @@ export const myProjects = [
         repoLink: 'https://github.com/poisoned-eden/schuper-duper-day-scheduler',
         deployedLink: 'https://poisoned-eden.github.io/schuper-duper-day-scheduler/',
         screenshot: schuperImg,
-        alt: 'Screenshot of a Work Day Scheduler.  A simple calendar app for scheduling your work day.  The date is displayed, with the time below it.  There are then rows for each hour, with different background colours showing if it is in the past, present, or future.  There are save buttons at the end of each row.'
+        alt: 'Screenshot of a Work Day Scheduler.  A simple calendar app for scheduling your work day.  The date is displayed, with the time below it.  There are then rows for each hour, with different background colours showing if it is in the past, present, or future.  There are save buttons at the end of each row.',
+        description: 'A simple timeblock work day scheduler, to help you plan out your day. Written using jquery and dayJS.'
     },
     {
         id: 4,
@@ -31,7 +34,8 @@ export const myProjects = [
         repoLink: 'https://github.com/poisoned-eden/riddle-me-this-code-quiz',
         deployedLink: 'https://poisoned-eden.github.io/riddle-me-this-code-quiz/',
         screenshot: '',
-        alt: ''
+        alt: '',
+        description: 'A code quiz that runs in the browser and tests JavaScript knowledge. Created to test my JS skills in the quest to become a Coding Bootcamp Graduate. Practicing coding assessments will be useful for the job search after graduation too!'
     },
     {
         id: 5,
@@ -39,7 +43,8 @@ export const myProjects = [
         repoLink: 'https://github.com/AMillsy/Visual-Vault',
         deployedLink: 'https://visual-vault-70f24eab589d.herokuapp.com/',
         screenshot: visualVaultImg,
-        alt: 'Screenshot of visual vault homepage.  Designed with developers in mind.'
+        alt: 'Screenshot of visual vault homepage.  Designed with developers in mind.',
+        description: 'a website that serves as a platform for developers to show off their project repositories. It also allows users to actively engage with other developers by giving reactions using emojis. This creates a fun environment for developers to interact with each other. Furthermore, we hope that the website can become a platform for employers to view projects of potential candidates.'
     },
     {
         id: 6,
@@ -47,6 +52,7 @@ export const myProjects = [
         repoLink: 'https://github.com/Robyn-Smith/The-Dev-Society',
         deployedLink: 'https://robyn-smith.github.io/The-Dev-Society/',
         screenshot: picturesqueImg,
-        alt: 'Art-deco cinema themed website for searching for movie information and saving them to a watchlist'
+        alt: 'Art-deco cinema themed website for searching for movie information and saving them to a watchlist',
+        description: 'Golden Age Cinema themed website for discovering films and saving them to your watchlist.'
     },
 ]
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-export default function Project({id, title, repoLink, deployedLink, screenshot, alt}) {
+export default function Project({id, title, repoLink, deployedLink, screenshot, alt, description}) {
     return (
-        <div key={id}>
-            <h4>{title}</h4>
-        </div>
+        <Card className='mt-5'>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {description}
+                </Card.Text>
+                <Row>
+                    
+                    <Col>
+                        <Button href={repoLink} target='_blank'>Link to Repo</Button>
+                    </Col>
+                    <Col>
+                        <Button href={deployedLink} target='_blank'>Link to deployed project</Button>
+                    </Col>
+                </Row>
+            </Card.Body>
+            <Card.Img src={screenshot} alt={alt} variant='bottom' />
+        </Card>
+
+        // <div key={id}>
+        //     <h4>{title}</h4
+        // </div>
     );
 }
