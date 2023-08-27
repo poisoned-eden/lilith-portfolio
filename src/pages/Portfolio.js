f0ecd29c47d7a41375eabbdfbe503863bf63a81a
poisoned-eden <poisoned.eden@gmail.com>
2023-08-27 15:35:25 +0100

    adds MyGithub component for reuse of profile info

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import MyGithub from '../components/MyGithub';
 
 export default function Portfolio() {
     return (
-        <p>Portfolio</p>  
+        <article>
+            <h3>Portfolio</h3>
+            <p>Have a look at a few of my projects.  More can be found on <MyGithub text="my github" /></p>
+        </article>
     );
 }
