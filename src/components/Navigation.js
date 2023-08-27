ef6cb24903d91636bba2d4b3f7677865139dd5e1
poisoned-eden <poisoned.eden@gmail.com>
2023-08-27 22:19:04 +0100

    Moves h1 out of navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,33 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default function Navigation({ currentPage, handlePageChange }) {
 	return (
-		<Nav fill> 
-			<Nav.Item>
-				<LinkContainer to="/">
-					<Nav.Link>About Me</Nav.Link>
-				</LinkContainer>
-			</Nav.Item>
-			<Nav.Item>
-				<LinkContainer to="/portfolio">
-					<Nav.Link>Portfolio</Nav.Link>
-				</LinkContainer>
-			</Nav.Item>
-			<Nav.Item>
-				<LinkContainer to="/cv">
-					<Nav.Link>CV</Nav.Link>
-				</LinkContainer>
-			</Nav.Item>
-			<Nav.Item>
-				<LinkContainer to="/contact">
-					<Nav.Link>Contact</Nav.Link>
-				</LinkContainer>
-			</Nav.Item>
-		</Nav>
-	);
+		<Navbar>
+			<Nav justify variant='underline' className='w-100'> 
+				<Nav.Item>
+					<LinkContainer to="/">
+						<Nav.Link>About Me</Nav.Link>
+					</LinkContainer>
+				</Nav.Item>
+				<Nav.Item>
+					<LinkContainer to="/portfolio">
+						<Nav.Link>Portfolio</Nav.Link>
+					</LinkContainer>
+				</Nav.Item>
+				<Nav.Item>
+					<LinkContainer to="/cv">
+						<Nav.Link>CV</Nav.Link>
+					</LinkContainer>
+				</Nav.Item>
+				<Nav.Item>
+					<LinkContainer to="/contact">
+						<Nav.Link>Contact</Nav.Link>
+					</LinkContainer>
+				</Nav.Item>
+			</Nav>
+		</Navbar>
+	)
 }
