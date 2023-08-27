225578a35659bb329ed23227cbef199a60b1589a
poisoned-eden <poisoned.eden@gmail.com>
2023-08-27 20:50:23 +0100

    adds links and copyright to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import MyGithub from './MyGithub';
+import Nav from 'react-bootstrap/Nav';
 
 export default function Footer() {
-    return (
-        <p>Footer</p>  
-    );
+	return (
+		<footer>
+			<Nav className="justify-content-center">
+				<Nav.Item>
+					<Nav.Link href="" target="_blank" disabled>
+						&copy; Lilith Miller-Fermor 2023.
+					</Nav.Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link
+						href="https://github.com/poisoned-eden"
+						target="_blank"
+					>
+						github
+					</Nav.Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link href="" target="_blank">
+						linkedin
+					</Nav.Link>
+				</Nav.Item>
+			</Nav>
+		</footer>
+	);
 }
