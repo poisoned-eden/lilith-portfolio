8c76ee47898b2173b6179bd4c1076a26fa1a6d2e
poisoned-eden <poisoned.eden@gmail.com>
2023-08-28 21:02:14 +0100

    adds skills

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -4,27 +4,48 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import cvPDF from '../assets/Lilith_Miller-Fermor_-_CV.pdf';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function CV() {
 	return (
 		<article>
-			<h2 className='mb-4'>Curriculum Vitae</h2>
-			<Row>
-				<Col xs={2} className=''>
-					<Button href={cvPDF} className='download'>
-						<DownloadIcon className='w-100' fill='white' />
+			<h2 className="mb-4">Curriculum Vitae</h2>
+			<Row xs={1} md={2}>
+				<Col md={2} className='d-flex align-items-center'>
+					<Button href={cvPDF} className="mb-3 download mx-auto">
+						<DownloadIcon fill="white" />
 						Download CV
 					</Button>
 				</Col>
-				<Col>
+				<Col md={10}>
 					<p>
-						Hit the button to download my full work CV.  If you would like information that is more tailored to an employment opportunity you have, please contact me directly.
+						Hit the button to download my full work CV. If you would
+						like information that is more tailored to an employment
+						opportunity you have, please contact me directly.
 					</p>
 					<p>Thank you for your interest!</p>
 				</Col>
 			</Row>
-			{/* TODO list of my proficiencies */}
-
+			<h3 className="mt-4">Skills</h3>
+			<ListGroup>
+				<Row>
+					<Col>
+						<ListGroup.Item>React</ListGroup.Item>
+						<ListGroup.Item>Node.js</ListGroup.Item>
+						<ListGroup.Item>Express.js</ListGroup.Item>
+						<ListGroup.Item>Jest</ListGroup.Item>
+						<ListGroup.Item>Git</ListGroup.Item>
+						<ListGroup.Item>NoSQL</ListGroup.Item>
+					</Col>
+					<Col>
+						<ListGroup.Item>SQL</ListGroup.Item>
+						<ListGroup.Item>JS</ListGroup.Item>
+						<ListGroup.Item>HTML & CSS</ListGroup.Item>
+						<ListGroup.Item>PHP</ListGroup.Item>
+						<ListGroup.Item>Python</ListGroup.Item>
+					</Col>
+				</Row>
+			</ListGroup>
 		</article>
 	);
 }
