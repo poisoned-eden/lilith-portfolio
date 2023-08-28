feb2e7346a90e8c31e7b4423b8ecaeaedb7573ed
poisoned-eden <poisoned.eden@gmail.com>
2023-08-28 15:13:02 +0100

    adds buttons to contact details

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import emailLogo from '../assets/images/email-logo.png';
-import phoneLogo from '../assets/images/phone-call.png';
-import githubLogo from '../assets/images/github-mark.png';
+import emailIcon from '../assets/icons/envelope-at.svg';
+import phoneIcon from '../assets/icons/telephone.svg';
+import githubLogo from '../assets/icons/github.svg';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
 
 export default function Contact() {
 	return (
@@ -11,16 +14,35 @@ export default function Contact() {
 			<h2>Contact Me</h2>
 			<div>
 				<h3>Contact Details</h3>
-				<address class="content">
+				<address>
 					<div>
-						<img src={emailLogo} alt="" />
-						<a href="mailto:dev@lilithmf.com">dev@lilithmf.com</a>
+							<ButtonGroup role="button" aria-label='email address' className='w-100'>
+								<Button href='mailto:hello@lilithmf.com'>
+									<img src={emailIcon} alt="Email Me" />
+								</Button>
+								<Button href='mailto:hello@lilithmf.com'> hello@lilithmf.com </Button>
+								{/* TODO add copy button */}
+							</ButtonGroup>
+					</div>
+					<div>
+						<ButtonGroup role="button" aria-label='phone number' className='w-100'>
+							<Button href='tel:+447557911251'>
+								<img src={phoneIcon} alt="Call Me" />
+							</Button>
+							<Button href='tel:+447557911251'> +44 7557 911 251</Button>
+							{/* TODO add copy button */}
+						</ButtonGroup>
+					</div>
+					<div>
+						<ButtonGroup role="button" aria-label='phone number' className='w-100'>
+							<Button href='https://github.com/poisoned-eden'>
+								<img src={githubLogo} alt="Check out my GitHub profile" />
+							</Button>
+							<Button href='https://github.com/poisoned-eden'>github.com/poisoned-eden</Button>
+							{/* TODO add copy button */}
+						</ButtonGroup>
 					</div>
 
-					<div>
-						<img src={phoneLogo} alt="" />
-						<a href="tel:+447557911251">+44 7557 911 251</a>
-					</div>
 					<div>
 						<img src={githubLogo} alt="" />
 						<a href="https://github.com/poisoned-eden">
@@ -33,8 +55,7 @@ export default function Contact() {
 				<h3>Send me a message</h3>
 				<Form>
 					<div class="alert alert-primary" role="alert">
-						This form is currently disabled. Please use the contact
-						details above.
+						This form is under construction.  In the meantime, it will create a custom mailto link that you can use to send me your entered message.
 					</div>
 					<fieldset disabled>
 						<Form.Group className="mb-3" controlId="formName">
